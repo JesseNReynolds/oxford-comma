e20165638cf38d27b3255efe68dcd327e99c7988
JesseNReynolds <jessenreynolds@gmail.com>
2020-10-01 12:48:24 -0600

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,26 @@
 def oxford_comma(array)
+    english = ""
+    length = array.length
 
+    if length == 1
+        english << "#{array[0]}"
+    end
+
+    if length == 2
+        english << "#{array[0]} and #{array[1]}"
+    end
+
+    array.each_with_index do |word, i|
+
+        if i + 1 < length && length > 2
+            english << "#{array[i]}, "
+        end
+
+        if i + 1  == length && length > 2
+            english << "and #{array[i]}"
+        end
+
+    end
+
+    return english
 end
